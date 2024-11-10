336c1bac0af5c19b40350cdcc5d7a1847e294407
Marie Mélody F <mariemelo@MacBook-Pro-de-Marie.local>
2024-11-10 15:24:24 +0100

    modif

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,16 +1,3 @@
-// export const SPOTIFY_CLIENT_ID = "cd91252789864440a432a0a8279b8e60";
-// export const SPOTIFY_REDIRECT_URI = "http://localhost:3000";
-
-// export const SPOTIFY_SCOPES = [
-//   "user-library-read",
-//   "playlist-read-private",
-//   "playlist-read-collaborative",
-//   "user-read-recently-played",
-//   "user-top-read",
-//   "user-read-email",
-//   "user-read-private",
-// ];
-
 export const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 export const SPOTIFY_REDIRECT_URI = process.env
   .REACT_APP_SPOTIFY_REDIRECT_URI as string;
